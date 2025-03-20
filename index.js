19469c0b5f89ea287272e6e22e420a17efc923ef
vladyslavZinchenko77 <vladyslav.zinchenko@gmail.com>
2025-03-20 13:32:29 +0200

    added task-9 and resolve conflict

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ function fibonacci(n) {
 }
 
 console.log(fibonacci(10)); // 55
+
+function removeDuplicates(arr) {
+  let result = [];
+
+  result = arr.filter((num, index, arr) => arr.indexOf(num) === index);
+  console.log(result);
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // [1, 2, 3, 4, 5]
+console.log(removeDuplicates([7, 8, 8, 9, 10, 10, 10])); // [7, 8, 9, 10]
+console.log(removeDuplicates([100, 200, 100, 300])); // [100, 200, 300]
+console.log(removeDuplicates([])); // []
